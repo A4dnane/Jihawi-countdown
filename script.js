0d64f70e4b6fb09553bb51da6f88762d14407349
Adnane <zerqaneadnane@gmail.com>
2025-04-20 22:49:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,33 @@
     js.src = "//www.tickcounter.com/static/js/loader.js";
     pjs.parentNode.insertBefore(js, pjs);
 }(document, "script", "tickcounter-sdk"));
+
+function switchTheme(theme) {
+  const themeStylesheet = document.getElementById('theme-stylesheet');
+
+  switch (theme) {
+      case 'blue':
+          themeStylesheet.setAttribute('href', 'style.css');
+          break;
+      case 'dark':
+          themeStylesheet.setAttribute('href', 'dark-theme.css');
+          break;
+      case 'pink':
+          themeStylesheet.setAttribute('href', 'pink-theme.css');
+          break;
+      case 'gold':
+          themeStylesheet.setAttribute('href', 'golden-theme.css');
+          break;
+      default:
+          themeStylesheet.setAttribute('href', 'style.css');
+  }
+
+  // Save the selected theme to localStorage
+  localStorage.setItem('theme', theme);
+}
+
+// Load the saved theme on page load
+window.onload = function() {
+  const savedTheme = localStorage.getItem('theme') || 'blue';
+  switchTheme(savedTheme);
+};
